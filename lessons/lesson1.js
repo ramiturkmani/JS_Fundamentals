707d183367daaaebe977cc370d2c2f2d2629d157
ramiturkmani <ramiturkmani1@gmail.com>
2023-07-26 13:47:01 -0500

    initial commit

---
+++ lessons/lesson1.js
@@ -0,0 +1,25 @@
+//Hello World
+console.log("Hello World!");
+
+//variables
+let firstName = "John"
+let lastName = "Smith"
+console.log(lastName);
+
+let age, dateOfBirth, sex
+age = 5
+sex = "Male"
+console.log(age);
+age = 6
+console.log(age);
+
+//constants
+const occupation = "Engineer"
+console.log(occupation);
+
+//data types
+let middleName = 'David' //string
+let ageOfBrother = 25 //number
+let isHeMarried = false //boolean
+let yearsMarried = null //null
+let numberOfCars = undefined //undefined
---
+++ lessons/lesson2.js
@@ -0,0 +1,6 @@
+//Concatination and Interpolation
+
+let price = 50
+let itemName = "Cup"
+let messageToPrint = `The price for your ${itemName} is ${price} dollars`
+console.log(messageToPrint);
