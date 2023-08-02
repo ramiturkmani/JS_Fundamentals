cce5389b49b367f593774185deae2881c0ecf89d
ramiturkmani <ramiturkmani1@gmail.com>
2023-08-01 23:58:44 -0500

    added lesson 9

--- helpers/printHelper.js
+++ helpers/printHelper.js
@@ -1,3 +1,13 @@
 export function printAge(age) {
     console.log(age);
 }
+
+export class CustomerDetails {
+    printFirstName(firstName){
+        console.log(firstName);
+    }
+
+    printFirstName(lastName){
+        console.log(lastName);
+    }
+}
---
+++ lessons/lesson9.js
@@ -0,0 +1,6 @@
+import { CustomerDetails } from "../helpers/printHelper.js";
+
+let customerDetails = new CustomerDetails();
+
+customerDetails.printFirstName('Steve');
+customerDetails.printLastName('Nash');
